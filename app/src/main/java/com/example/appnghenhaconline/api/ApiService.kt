e04c26a573c0acbc7783bbfc8b35ef7ed0edf0b9
Tan Nguyen <tannguyenvnkg@gmail.com>
2021-10-16 17:56:39 +0700

    sửa kích thước ảnh trong listview listmusic.xml

--- app/src/main/java/com/example/appnghenhaconline/api/ApiService.kt
+++ app/src/main/java/com/example/appnghenhaconline/api/ApiService.kt
@@ -14,7 +14,7 @@ interface ApiService {
     fun getListSong() : Call<DataSong>
 
     companion object{
-        private val baseUrl = "http://localhost:3000/"
+        private val baseUrl = "http://192.168.10.62:3000/"
         private val gson: Gson = GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create()
         val apiService: ApiService = Retrofit.Builder()
             .baseUrl(baseUrl).addConverterFactory(GsonConverterFactory.create(gson))
